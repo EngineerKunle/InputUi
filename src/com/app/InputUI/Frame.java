16f910de3bbbedbf2deb6ef1c9e82f3005e69ca2
Kunle Ogunjimi <kunle.ogunjimi@cohaesus.co.uk>
2016-06-12 16:59:40 +0100

    button completed

--- src/com/app/InputUI/Frame.java
+++ src/com/app/InputUI/Frame.java
@@ -3,16 +3,20 @@ package com.app.InputUI;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class Frame extends JFrame{
 	
-	private int width = 300;
+	private int width = 500;
 	private int height = 200;
 	private JTextField jField;
+	private JButton clickMe;
 	
 	public Frame(String title){
 		super(title);
@@ -20,27 +24,37 @@ public class Frame extends JFrame{
 	    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 	    setPanel();
 	    add(setPanel());
-	    setVisible(true);
 	}
 	//initialize the text area
 	private JTextField initTextArea(){
-		System.out.println("Jtextfield called and init");
+		
 		jField = new JTextField(20);
 		jField.setText("test");
-		//jField.setBounds(150,100,100,10);
+		jField.setBounds(150,100,100,10);
 		jField.setVisible(true);
 		return jField;
 	}
 	
+	//click event for us 
+	ActionListener clickEvent = new ActionListener(){
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("we have been clicked");
+		}	
+	};
+	
 	//call to create Panel
 	private JPanel setPanel(){
-		System.out.println("setpanel called and init");
+		
 		JPanel jpanel = new JPanel(new GridBagLayout());
+		clickMe = new JButton("Update Text");
+		clickMe.addActionListener(clickEvent);
 		GridBagConstraints cts = new GridBagConstraints();
 		cts.anchor = GridBagConstraints.WEST;
-		cts.gridx = 0;
+		cts.gridx = 1000;
 		cts.gridy = 0;
-		jpanel.setBackground(Color.blue);
+		jpanel.setBackground(Color.GRAY);
+		jpanel.add(clickMe);
 		jpanel.add(initTextArea(), cts);
 		jpanel.setVisible(true);
 		return jpanel;
