78109b229cdbdf58ea446a41f3b8f1db385a7823
Kunle Ogunjimi <kunle.ogunjimi@cohaesus.co.uk>
2016-06-12 18:10:25 +0100

    response view depending on input text updated

--- src/com/app/InputUI/Frame.java
+++ src/com/app/InputUI/Frame.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -15,8 +16,10 @@ public class Frame extends JFrame{
 	
 	private int width = 500;
 	private int height = 200;
-	private JTextField jField;
+	private JTextField jField = new JTextField(20);
+	private JLabel jLable = new JLabel();
 	private JButton clickMe;
+	private String data;
 	
 	public Frame(String title){
 		super(title);
@@ -27,35 +30,51 @@ public class Frame extends JFrame{
 	}
 	//initialize the text area
 	private JTextField initTextArea(){
-		
-		jField = new JTextField(20);
-		jField.setText("test");
+
+		//jField.setText("test");
 		jField.setBounds(150,100,100,10);
 		jField.setVisible(true);
+		data = jField.getText();
 		return jField;
 	}
 	
-	//click event for us 
-	ActionListener clickEvent = new ActionListener(){
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			System.out.println("we have been clicked");
-		}	
-	};
 	
 	//call to create Panel
 	private JPanel setPanel(){
 		
 		JPanel jpanel = new JPanel(new GridBagLayout());
+		jpanel.setBackground(Color.GRAY);
 		clickMe = new JButton("Update Text");
-		clickMe.addActionListener(clickEvent);
+
+		
 		GridBagConstraints cts = new GridBagConstraints();
 		cts.anchor = GridBagConstraints.WEST;
-		cts.gridx = 1000;
+		cts.gridx = 1;
 		cts.gridy = 0;
-		jpanel.setBackground(Color.GRAY);
-		jpanel.add(clickMe);
 		jpanel.add(initTextArea(), cts);
+		
+		GridBagConstraints c = new GridBagConstraints();
+		c.anchor = GridBagConstraints.EAST;
+		c.gridx = 0;
+		c.gridy = 0;
+		jpanel.add(clickMe, c);
+		
+		GridBagConstraints labelPlace = new GridBagConstraints();
+		labelPlace.anchor = GridBagConstraints.WEST;
+		labelPlace.gridx = 1;
+		labelPlace.gridy = 1;
+		jpanel.add(jLable, labelPlace);
+		
+		//event listener
+		clickMe.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				data = jField.getText();
+				//System.out.println(data);
+				jLable.setText(data);
+			}	
+		});
+	
 		jpanel.setVisible(true);
 		return jpanel;
 	}
